134aead601c6875a79d33678b20acc330fb265dd
Abdul Al-rahman Mohammed Hamdan <ahamdan4@uwo.ca>
2025-06-11 19:26:44 -0400

    .

--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 
 
-router.get('/', (req, res) => {
-    console.log('get request');
+
+router.get('/', async (req, res) => {
+    try {
+        const forms = await prisma.formSubission.findMany();
+        res.status(200).json(forms);
+    } catch (error) {
+        res.status(500).json({error: 'Failed to fetch forms'});
+    }
 })
 
 router.post('/', (req, res)=> {
